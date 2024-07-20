0b2b72726c93ae5a79afca7f96546c40b15fbfa7
Sandeep0928 <sandeepnishad1028@gmail.com>
2024-07-20 21:06:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7177782150:AAEDujBBeplqlr57R2mMnkH9K_AWew0_99g")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Sandeep")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6767689106")
 
 
 
